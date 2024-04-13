815c8f959743b22339720c807c9017a09ede0650
John Park <horaeng0.0@gmail.com>
2024-04-13 11:48:14 -0300

    Update the comment

--- mock-testing-enviornment/src/app/submission/actions.tsx
+++ mock-testing-enviornment/src/app/submission/actions.tsx
@@ -5,5 +5,5 @@ export async function action(_: string | null, formData: FormData) {
   const machineId = formData.get("machineId");
   const force = formData.get("force");
 
-  return `Teste de ruptura do corpo ${testBodyId} na máquina ${machineId} com carga de ${force}N salvo com sucesso!`;
+  return `Teste de ruptura do corpo ${testBodyId} na máquina ${machineId} com carga de ${force} kN salvo com sucesso!`;
 }
