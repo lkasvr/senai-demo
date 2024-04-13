9fe6b20ef3204d3642375aee4461391db4e0364f
lkasvr <lkasvr@outlook.com>
2024-04-13 12:43:25 -0300

    remove spare class to check whether json file tests was updated and centralize logic on JsonTestsManager class, names refactor to be more descriptive

---
+++ lab-integration/src/modules/tools/JsonFileTestsManager.ts
@@ -0,0 +1,29 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { testsResult } from '../types';
+
+export default class JsonFileTestsManager {
+  private filePath = path.join(process.cwd(), '../test-results.json');
+  private dataset: testsResult[] = [];
+
+    constructor(firstDataset: testsResult[]) {
+    this.dataset = firstDataset;
+  }
+
+  async readJsonTestsResultFile(): Promise<testsResult[]> {
+    const results = await fs.readFile(this.filePath, 'utf8');
+    return JSON.parse(results);
+  }
+
+  async writeJsonTestFile(newTestsResults: testsResult): Promise<void> {
+    const lastDataset = await this.readJsonTestsResultFile();
+    const updatedFileContents = JSON.stringify([...lastDataset, newTestsResults], null, 2);
+    await fs.writeFile(this.filePath, updatedFileContents);
+  }
+
+    isTestsResultsFileUpdated(newDataset: testsResult[]): boolean {
+    const flag = this.dataset.length !== newDataset.length;
+    this.dataset = newDataset;
+    return flag;
+  }
+}
