ce6619165663ba3d97c8d5397488e690c6107ed1
John Park <horaeng0.0@gmail.com>
2024-04-13 14:37:25 -0300

    Add file system operations to save test results in a JSON database. Necessary for persistence.

--- mock-testing-environment/src/app/submission/page.tsx
+++ mock-testing-environment/src/app/submission/page.tsx
@@ -1,3 +1,5 @@
+import { promises as fs } from "fs";
+import path from "path";
 import ResultSubmission from "./result-submission";
 
 export default function Page() {
@@ -12,7 +14,20 @@ export default function Page() {
   }) => {
     "use server";
 
-    console.log("here");
+    const filePath = path.join(process.cwd(), "../database.json");
+    const stringifiedDatabase = await fs.readFile(filePath, "utf8");
+    const database = JSON.parse(stringifiedDatabase);
+
+    const newResult = {
+      Id_do_Corpo_de_Prova: testBodyId,
+      Nome_da_Maquina: machineId,
+      Forca: force,
+    };
+
+    database.push(newResult);
+
+    await fs.writeFile(filePath, JSON.stringify(database, null, 2), "utf8");
+
     return `Teste de ruptura do corpo ${testBodyId} na máquina ${machineId} com carga de ${force} kN salvo com sucesso!`;
   };
 
