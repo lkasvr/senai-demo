29b00ed6a7bff8d58588616026c4e5525560f094
lkasvr <lkasvr@outlook.com>
2024-04-14 19:55:55 -0300

    fix linters problems

--- lab-integration/src/main.ts
+++ lab-integration/src/main.ts
@@ -1,20 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import "dotenv/config";
 
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 
-import JsonFileTestsManager from './modules/tools/JsonFileTestsManager';
-import { ITestsResult } from './modules/types';
-import FulfillmentAutomation from './modules/tools/FulfillmentAutomation';
-import { DataBaseService } from './modules/tools/DataBaseService';
+import JsonFileTestsManager from "./modules/tools/JsonFileTestsManager";
+import { ITestsResult } from "./modules/types";
+import FulfillmentAutomation from "./modules/tools/FulfillmentAutomation";
+import { DataBaseService } from "./modules/tools/DataBaseService";
 
 (async () => {
   const databaseService = new DataBaseService();
   const firstDataset = await fs.readFile(
-    path.join(process.cwd(), '../test-results.json'),
+    path.join(process.cwd(), "../test-results.json"),
     {
-      encoding: 'utf8',
+      encoding: "utf8",
     },
   );
 
@@ -23,37 +22,42 @@ import { DataBaseService } from './modules/tools/DataBaseService';
   );
   const fulfillmentAutomation = await new FulfillmentAutomation().init();
 
-  const isDevEnv = process.env.NODE_ENV === 'dev';
+  const isDevEnv = process.env.NODE_ENV === "dev";
 
-  if(isDevEnv) console.info('SCRIPT IS RUNNING...\n');
-  setInterval(async () => {
-    const dataset = await jsonFileTestsManager.readJsonTestsResultFile();
-    if (jsonFileTestsManager.isTestsResultsFileUpdated(dataset)) {
-      if(isDevEnv) {
-        console.info('\n-----------------------------------------------');
-        console.info(`[${new Date()}] \n ARQUIVO DE TESTE ATUALIZADO`);
-        console.info('-----------------------------------------------\n');
+  if (isDevEnv) console.info("SCRIPT IS RUNNING...\n");
+  setInterval(
+    async () => {
+      const dataset = await jsonFileTestsManager.readJsonTestsResultFile();
+      if (jsonFileTestsManager.isTestsResultsFileUpdated(dataset)) {
+        if (isDevEnv) {
+          console.info("\n-----------------------------------------------");
+          console.info(`[${new Date()}] \n ARQUIVO DE TESTE ATUALIZADO`);
+          console.info("-----------------------------------------------\n");
+        }
+
+        const lastTestResult = jsonFileTestsManager.getLastTestResult();
+        await fulfillmentAutomation.registerTestResult(lastTestResult);
+
+        jsonFileTestsManager.setDataset(dataset);
+
+        if (isDevEnv) {
+          console.info("\n-----------------------------------------------");
+          console.info(`Data: ${lastTestResult.Data}`);
+          console.info(`ID da Máquina: ${lastTestResult.Id_da_Maquina}`);
+          console.info(
+            `ID do Corpo da Prova: ${lastTestResult.Id_do_Corpo_de_Prova}`,
+          );
+          console.info(`Força: ${lastTestResult.Forca}`);
+          console.info("-----------------------------------------------\n");
+        }
+
+        if (
+          isDevEnv &&
+          databaseService.isDBUpdated(await databaseService.find())
+        )
+          console.info("BANCO DE DADOS ATUALIZADO COM SUCESSO");
       }
-
-      const lastTestResult = jsonFileTestsManager.getLastTestResult();
-      await fulfillmentAutomation.registerTestResult(lastTestResult);
-
-      jsonFileTestsManager.setDataset(dataset);
-
-      if(isDevEnv) {
-        console.info('\n-----------------------------------------------');
-        console.info(`Data: ${lastTestResult.Data}`);
-        console.info(`ID da Máquina: ${lastTestResult.Id_da_Maquina}`);
-        console.info(
-          `ID do Corpo da Prova: ${lastTestResult.Id_do_Corpo_de_Prova}`,
-        );
-        console.info(`Força: ${lastTestResult.Forca}`);
-        console.info('-----------------------------------------------\n');
-      }
-
-      if (isDevEnv && databaseService.isDBUpdated(await databaseService.find()))
-        console.info('BANCO DE DADOS ATUALIZADO COM SUCESSO');
-    }
-  }, Number(process.env.CHECKTESTSFILEINTERVAL) * 1000);
-
+    },
+    Number(process.env.CHECKTESTSFILEINTERVAL) * 1000,
+  );
 })();
--- lab-integration/src/modules/types.ts
+++ lab-integration/src/modules/types.ts
@@ -3,16 +3,16 @@ export interface ITestsResult {
   Id_do_Teste: string;
   Id_do_Corpo_de_Prova: string;
   Data: string;
-  Tipo_do_Ensaio: 'Cilíndrico';
+  Tipo_do_Ensaio: "Cilíndrico";
   Dimensoes: [
     {
-      Descricao: 'Diâmetro';
+      Descricao: "Diâmetro";
       Valor: string;
     },
   ];
   Forca: string;
   Tensão: string;
-  Ruptura: 'ND';
+  Ruptura: "ND";
 }
 
 export interface TestEntity {
