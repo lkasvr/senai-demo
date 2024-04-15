b0c75d3d1d934d828be380ac02debbbd37aefcca
lkasvr <lkasvr@outlook.com>
2024-04-15 11:53:43 -0300

    move change check event to outside the task, remove useless await for selectors

--- lab-integration/src/main.ts
+++ lab-integration/src/main.ts
@@ -3,17 +3,16 @@ import * as lockfile from "proper-lockfile";
 import path from "path";
 import { queue } from "async";
 import puppeteer from "puppeteer";
-
 import { DateTime } from "luxon";
-import { TestsResult } from "./types";
+import { Machines, TestsResult } from "./types";
 
 const filePath = path.join(process.cwd(), "../test-results.json");
 
-const machines = new Map<string, string>([
-  ["5534", "1 - Prensa PRE-01"],
-  ["5533", "2 - Prensa PRE-02"],
-  ["5532", "3 - Prensa PRE-03"],
-]);
+const machines: Machines = {
+  5534: "1 - Prensa PRE-01",
+  5533: "2 - Prensa PRE-02",
+  5532: "3 - Prensa PRE-03",
+};
 
 (async () => {
   console.info("SCRIPT IS RUNNING...");
@@ -25,66 +24,61 @@ const machines = new Map<string, string>([
   await page.setViewport({ width: 720, height: 720 });
   await page.goto("http://localhost:3000/submission");
 
-  const queueTask = queue(
-    async (task: { eventType: WatchEventType }, callback) => {
-      try {
-        if (task.eventType === "change") {
-          await lockfile.lock(filePath);
-          const stringifiedDataset = await fs.readFile(filePath, "utf8");
-          const parsedDataset = JSON.parse(stringifiedDataset) as TestsResult[];
-          const dataset = parsedDataset
-            .map((testResult) => {
-              const Data = DateTime.fromFormat(
-                testResult.Data,
-                "dd/LL/yyyy, HH:mm:ss",
-                { locale: "pt-BR" },
-              );
-
-              return {
-                ...testResult,
-                Data,
-              };
-            })
-            .filter((testResult) => testResult.Data.isValid);
-
-          const newTestsResults = dataset.filter(
-            (testResult) =>
-              testResult.Data.diff(lastTestResultDate).milliseconds > 0,
+  const queueTask = queue(async (task, callback) => {
+    try {
+      await lockfile.lock(filePath);
+      const stringifiedDataset = await fs.readFile(filePath, "utf8");
+      const parsedDataset = JSON.parse(stringifiedDataset) as TestsResult[];
+      const dataset = parsedDataset
+        .map((testResult) => {
+          const Data = DateTime.fromFormat(
+            testResult.Data,
+            "dd/LL/yyyy, HH:mm:ss",
+            { locale: "pt-BR" },
           );
-          if (newTestsResults.length > 0) {
-            lastTestResultDate = dataset.sort(
-              (a, b) => b.Data.diff(a.Data).milliseconds,
-            )[0].Data as DateTime<true>;
 
-            for (const testResult of newTestsResults) {
-              await page.type("#testBodyId", testResult.Id_do_Corpo_de_Prova);
-              await page.click("button#search");
+          return {
+            ...testResult,
+            Data,
+          };
+        })
+        .filter((testResult) => testResult.Data.isValid);
 
-              await page.waitForSelector("select#machineId");
-              await page.select(
-                "select#machineId",
-                machines.get(testResult.Id_da_Maquina) ?? "",
-              );
+      const newTestsResults = dataset.filter(
+        (testResult) =>
+          testResult.Data.diff(lastTestResultDate).milliseconds > 0,
+      );
 
-              await page.waitForSelector("button#submit");
-              await page.type(
-                "#force",
-                parseFloat(testResult.Forca).toFixed(2),
-              );
-              await page.click("button#submit");
+      if (newTestsResults.length > 0) {
+        lastTestResultDate = newTestsResults.sort(
+          (a, b) => b.Data.diff(a.Data).milliseconds,
+        )[0].Data as DateTime<true>;
 
-              await page.waitForSelector("button#search");
-            }
-          }
+        for (const testResult of newTestsResults) {
+          await page.type("#testBodyId", testResult.Id_do_Corpo_de_Prova);
+          await page.click("button#search");
+
+          await page.waitForSelector("select#machineId");
+          await page.select(
+            "select#machineId",
+            machines[testResult.Id_da_Maquina] ?? "",
+          );
+
+          await page.type("#force", parseFloat(testResult.Forca).toFixed(2));
+
+          await page.click("button#submit");
         }
-      } finally {
-        await lockfile.unlock(filePath);
       }
+    } finally {
+      await lockfile.unlock(filePath);
+    }
 
-      callback();
-    },
-    1,
+    callback();
+  }, 1);
+
+  watch(
+    filePath,
+    async (eventType) =>
+      eventType === "change" && queueTask.push({ eventType }),
   );
-
-  watch(filePath, async (eventType) => queueTask.push({ eventType }));
 })();
--- lab-integration/src/types.ts
+++ lab-integration/src/types.ts
@@ -14,3 +14,7 @@ export type TestsResult = {
   Tensão: string;
   Ruptura: "ND";
 };
+
+export type Machines = {
+  [key: string]: string;
+};
