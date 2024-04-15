3eb1a9745814e70ec86d4eec00b70df3f6e8c3a7
lkasvr <lkasvr@outlook.com>
2024-04-14 23:53:00 -0300

    testing debug

--- lab-integration/src/main.ts
+++ lab-integration/src/main.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 import { promises as fs, watch } from "fs";
 import path from "path";
 import { TestsResult } from "./types";
@@ -9,14 +11,18 @@ const filePath = path.join(process.cwd(), "../test-results.json");
 
   const stringifiedDataset = await fs.readFile(filePath, "utf8");
   const dataset = JSON.parse(stringifiedDataset) as TestsResult[];
-  let lastTestResultDate: Date = getLastTestsResulstDate(dataset);
+  let lastTestResultDate: DateTime = getLastTestsResulstDate(dataset);
 
   watch(filePath, (eventType) => {
     if (eventType === "change") {
       const newTestsResults = dataset.filter(
-        (testResult) => convertToDate(testResult.Data) > lastTestResultDate,
+        (testResult) => {
+          console.info(convertToDate(testResult.Data), lastTestResultDate)
+          console.info(convertToDate(testResult.Data) > lastTestResultDate)
+         return convertToDate(testResult.Data).diff(lastTestResultDate).milliseconds > 0;
+        }
       );
-      lastTestResultDate = getLastTestsResulstDate(dataset);
+      lastTestResultDate = getLastTestsResulstDate(newTestsResults, lastTestResultDate);
 
       console.info(newTestsResults);
       console.info(lastTestResultDate);
@@ -24,26 +30,27 @@ const filePath = path.join(process.cwd(), "../test-results.json");
   });
 })();
 
-function getLastTestsResulstDate(dataset: TestsResult[]): Date {
-  if (dataset.length === 0) return new Date();
+function getLastTestsResulstDate(dataset: TestsResult[], lastTestResultDate?: DateTime): DateTime {
+  if(dataset.length === 0)
+    return lastTestResultDate || DateTime.now();
+
   const orderedNewTestsResults = dataset.sort(
-    (a, b) => convertToDate(a.Data).getTime() - convertToDate(b.Data).getTime(),
-  );
+    (a, b) => {
+      const dateA = convertToDate(a.Data);
+      const dateB = convertToDate(b.Data);
+      return dateA.diff(dateB).milliseconds;
+    });
+
+    console.info(convertToDate(
+    orderedNewTestsResults[orderedNewTestsResults.length - 1].Data,
+  ))
+
+
   return convertToDate(
     orderedNewTestsResults[orderedNewTestsResults.length - 1].Data,
   );
 }
 
-function convertToDate(date: string): Date {
-  const [day, month, rest] = date.split("/");
-  const [year, time] = rest.split(",");
-  const [h, m, s] = time.split(":");
-  return new Date(
-    parseInt(year.trim()),
-    parseInt(month),
-    parseInt(day),
-    parseInt(h),
-    parseInt(m),
-    parseInt(s),
-  );
+function convertToDate(date: string): DateTime {
+  return DateTime.fromFormat(date, 'dd/LL/yyyy, HH:mm:ss', { locale: 'pt-BR' });
 }
