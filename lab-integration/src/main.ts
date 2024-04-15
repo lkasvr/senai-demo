047acf535a4dae575306e5b0978600e8b0eb27cb
lkasvr <lkasvr@outlook.com>
2024-04-14 22:12:14 -0300

    Watch json file tests results and register most recent date from a teste result

--- lab-integration/src/main.ts
+++ lab-integration/src/main.ts
@@ -1,63 +1,45 @@
-import "dotenv/config";
-
-import { promises as fs } from "fs";
+import { promises as fs, watch } from "fs";
 import path from "path";
+import { ITestsResult } from "./types";
 
-import JsonFileTestsManager from "./modules/tools/JsonFileTestsManager";
-import { ITestsResult } from "./modules/types";
-import FulfillmentAutomation from "./modules/tools/FulfillmentAutomation";
-import { DataBaseService } from "./modules/tools/DataBaseService";
+const filePath = path.join(process.cwd(), "../test-results.json");
+
 
 (async () => {
-  const databaseService = new DataBaseService();
-  const firstDataset = await fs.readFile(
-    path.join(process.cwd(), "../test-results.json"),
-    {
-      encoding: "utf8",
-    },
-  );
+  console.info('SCRIPT IS RUNNING...');
 
-  const jsonFileTestsManager = new JsonFileTestsManager(
-    JSON.parse(firstDataset) as ITestsResult[],
-  );
-  const fulfillmentAutomation = await new FulfillmentAutomation().init();
+  const stringifiedDataset = await fs.readFile(filePath, "utf8");
+  const dataset = JSON.parse(stringifiedDataset) as  ITestsResult[];
+  let lastTestResultDate: Date = getLastTestsResulstDate(dataset);
 
-  const isDevEnv = process.env.NODE_ENV === "dev";
+  watch(filePath, (eventType) => {
+    if (eventType === "change") {
 
-  if (isDevEnv) console.info("SCRIPT IS RUNNING...\n");
-  setInterval(
-    async () => {
-      const dataset = await jsonFileTestsManager.readJsonTestsResultFile();
-      if (jsonFileTestsManager.isTestsResultsFileUpdated(dataset)) {
-        if (isDevEnv) {
-          console.info("\n-----------------------------------------------");
-          console.info(`[${new Date()}] \n ARQUIVO DE TESTE ATUALIZADO`);
-          console.info("-----------------------------------------------\n");
-        }
+      const newTestsResults = dataset.filter((testResult) => convertToDate(testResult.Data) > lastTestResultDate);
 
-        const lastTestResult = jsonFileTestsManager.getLastTestResult();
-        await fulfillmentAutomation.registerTestResult(lastTestResult);
+      if(newTestsResults.length > 0)
+        lastTestResultDate = getLastTestsResulstDate(dataset);
 
-        jsonFileTestsManager.setDataset(dataset);
-
-        if (isDevEnv) {
-          console.info("\n-----------------------------------------------");
-          console.info(`Data: ${lastTestResult.Data}`);
-          console.info(`ID da Máquina: ${lastTestResult.Id_da_Maquina}`);
-          console.info(
-            `ID do Corpo da Prova: ${lastTestResult.Id_do_Corpo_de_Prova}`,
-          );
-          console.info(`Força: ${lastTestResult.Forca}`);
-          console.info("-----------------------------------------------\n");
-        }
-
-        if (
-          isDevEnv &&
-          databaseService.isDBUpdated(await databaseService.find())
-        )
-          console.info("BANCO DE DADOS ATUALIZADO COM SUCESSO");
-      }
-    },
-    Number(process.env.CHECKTESTSFILEINTERVAL) * 1000,
-  );
+      console.info(lastTestResultDate);
+    }
+  });
 })();
+
+function getLastTestsResulstDate(dataset: ITestsResult[]): Date {
+  const orderedNewTestsResults = dataset.sort((a, b) => convertToDate(a.Data).getTime() - convertToDate(b.Data).getTime());
+  return convertToDate(orderedNewTestsResults[orderedNewTestsResults.length - 1].Data);
+}
+
+function convertToDate(date: string): Date {
+    const [day, month, rest] = date.split("/");
+    const [year, time] = rest.split(",");
+    const [h, m, s] = time.split(":");
+    return new Date(
+      parseInt(year.trim()),
+      parseInt(month),
+      parseInt(day),
+      parseInt(h),
+      parseInt(m),
+      parseInt(s),
+    );
+  }
