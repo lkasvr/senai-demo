e0a32d52ffae88795edd1360cda998aea4fc4d4c
lkasvr <lkasvr@outlook.com>
2024-04-14 22:22:06 -0300

    ran linter fix command

--- lab-integration/src/main.ts
+++ lab-integration/src/main.ts
@@ -4,20 +4,20 @@ import { ITestsResult } from "./types";
 
 const filePath = path.join(process.cwd(), "../test-results.json");
 
-
 (async () => {
-  console.info('SCRIPT IS RUNNING...');
+  console.info("SCRIPT IS RUNNING...");
 
   const stringifiedDataset = await fs.readFile(filePath, "utf8");
-  const dataset = JSON.parse(stringifiedDataset) as  ITestsResult[];
+  const dataset = JSON.parse(stringifiedDataset) as ITestsResult[];
   let lastTestResultDate: Date = getLastTestsResulstDate(dataset);
 
   watch(filePath, (eventType) => {
     if (eventType === "change") {
+      const newTestsResults = dataset.filter(
+        (testResult) => convertToDate(testResult.Data) > lastTestResultDate,
+      );
 
-      const newTestsResults = dataset.filter((testResult) => convertToDate(testResult.Data) > lastTestResultDate);
-
-      if(newTestsResults.length > 0)
+      if (newTestsResults.length > 0)
         lastTestResultDate = getLastTestsResulstDate(dataset);
 
       console.info(lastTestResultDate);
@@ -26,20 +26,24 @@ const filePath = path.join(process.cwd(), "../test-results.json");
 })();
 
 function getLastTestsResulstDate(dataset: ITestsResult[]): Date {
-  const orderedNewTestsResults = dataset.sort((a, b) => convertToDate(a.Data).getTime() - convertToDate(b.Data).getTime());
-  return convertToDate(orderedNewTestsResults[orderedNewTestsResults.length - 1].Data);
+  const orderedNewTestsResults = dataset.sort(
+    (a, b) => convertToDate(a.Data).getTime() - convertToDate(b.Data).getTime(),
+  );
+  return convertToDate(
+    orderedNewTestsResults[orderedNewTestsResults.length - 1].Data,
+  );
 }
 
 function convertToDate(date: string): Date {
-    const [day, month, rest] = date.split("/");
-    const [year, time] = rest.split(",");
-    const [h, m, s] = time.split(":");
-    return new Date(
-      parseInt(year.trim()),
-      parseInt(month),
-      parseInt(day),
-      parseInt(h),
-      parseInt(m),
-      parseInt(s),
-    );
-  }
+  const [day, month, rest] = date.split("/");
+  const [year, time] = rest.split(",");
+  const [h, m, s] = time.split(":");
+  return new Date(
+    parseInt(year.trim()),
+    parseInt(month),
+    parseInt(day),
+    parseInt(h),
+    parseInt(m),
+    parseInt(s),
+  );
+}
